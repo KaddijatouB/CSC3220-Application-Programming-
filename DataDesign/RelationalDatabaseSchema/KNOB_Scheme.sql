ddd77cc8202530fa68c67e2df216ca366b57808d
Kaddijatou Baldeh <baldehk@spu.edu>
2023-02-19 12:50:30 -0800

    Updated data design docs

---
+++ DataDesign/RelationalDatabaseSchema/KNOB_Scheme.sql
@@ -0,0 +1,32 @@
+-- File generated with SQLiteStudio 
+
+-- Table: User Info
+CREATE TABLE "userInfo" (
+	"userID"	TEXT,
+	"password"	TEXT,
+	PRIMARY KEY("userID")
+);
+
+-- Table: User profile
+CREATE TABLE "UserProfile" (
+	"monthlyIncome"	TEXT,
+	"UserID"	TEXT UNIQUE,
+	"userName"	TEXT,
+	"email"	TEXT,
+	"address"	TEXT,
+	FOREIGN KEY("UserID") REFERENCES userInfo("userID")
+);
+
+
+-- Table: Display Statics 
+CREATE TABLE "DisplayStats" (
+	"pieChart"	NUMERIC,
+	"statSummary"	INTEGER
+);
+
+-- Table: Expense
+CREATE TABLE "Expense" (
+	"expenseName"	TEXT,
+	"amount"	TEXT,
+	"date"	INTEGER
+);
